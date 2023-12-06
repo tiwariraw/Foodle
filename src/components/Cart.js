cfc835a06d86f84b98b3cf7004847075ddd0c899
Ashish <ashishiit9@gmail.com>
2023-12-06 21:47:10 +0530

    modify image

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -45,7 +45,7 @@ const Cart = () => {
                 cartItems.length == 0
                     ?
                     <div className='empty-cart'>
-                        <img src='https://images.pexels.com/photos/296916/pexels-photo-296916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='food-img' />
+                        <img src='https://images.unsplash.com/photo-1557821552-17105176677c?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='food-img' />
                         <h2>Your cart is empty</h2>
                         <p>You can go to home page to view more restaurants</p>
                         <Link to="/" className='home'>See Restaurants near you</Link>
